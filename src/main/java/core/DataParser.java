088bf1824ba45fe5c06c1f2add79273f2b44d52b
onetuks <onetuks@naver.com>
2025-07-01 20:18:49 +0900

    Feat: DT 정보 -> DTO 파서 기능 구현

--- src/main/java/core/DataParser.java
+++ src/main/java/core/DataParser.java
@@ -1,4 +1,33 @@
 package core;
 
+import dto.XDataType;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class DataParser {
+
+  public static final String NA = "N/A";
+  public static final String NEW_LINE = "\n";
+  private static final String BLANK = "";
+
+  public List<XDataType> parseData(String nameText, String descriptionText) {
+    String[] names = nameText.trim().split("\n");
+    String[] descriptions = descriptionText.split("\n");
+
+    return IntStream.range(0, names.length)
+        .filter(i -> {
+          String name = names[i].trim().replaceAll(NA, BLANK);
+          names[i] = name;
+          return !name.isEmpty();
+        })
+        .mapToObj(i -> {
+          String name = names[i];
+          String description = descriptions.length <= i
+              ? ""
+              : descriptions[i].trim().replaceAll(NA, BLANK);
+          return new XDataType(name, description);
+        })
+        .collect(Collectors.toList());
+  }
 }
