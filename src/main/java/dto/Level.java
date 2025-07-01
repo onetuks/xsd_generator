29333a38e399775d3d595095d7f8e655b6c66063
onetuks <onetuks@naver.com>
2025-07-01 20:12:05 +0900

    Feat: DTO 객체 선언

--- src/main/java/dto/Level.java
+++ src/main/java/dto/Level.java
@@ -1,4 +1,9 @@
 package dto;
 
 public interface Level {
+  int FIRST = 1;
+  int SECOND = 2;
+  int THIRD = 3;
+  int FOURTH = 4;
+  int FIFTH = 5;
 }
--- src/main/java/dto/XDataType.java
+++ src/main/java/dto/XDataType.java
@@ -1,4 +1,117 @@
 package dto;
 
+import java.util.Objects;
+import model.vo.Attribute;
+import model.vo.Category;
+import model.vo.Occurrence;
+import model.vo.Type;
+
 public class XDataType {
+
+  public static final String STATEMENT = "StatementName";
+  public static final String DB_TABLE_NAME = "dbTableName";
+  public static final String ACTION = "action";
+  public static final String ACCESS = "access";
+  public static final String KEY = "key";
+  public static final String TABLE = "table";
+  public static final String ROW = "row";
+
+  private final int level;
+  private final String name;
+  private final String description;
+  private Category category;
+  private final Type type;
+  private final Occurrence occurrence;
+
+  public XDataType(String dataTypeName) {
+    this.level = getProperLevel(dataTypeName);
+    this.name = dataTypeName;
+    this.description = "";
+    this.category = Category.COMPLEX_TYPE;
+    this.type = null;
+    this.occurrence = null;
+  }
+
+  public XDataType(String name, String description) {
+    this.level = getProperLevel(name);
+    this.name = name;
+    this.description = description;
+    this.category = getProperCategory(name);
+    this.type = Type.STRING;
+    this.occurrence = getProperOccurrence(name);
+  }
+
+  private int getProperLevel(String name) {
+    if (Objects.equals(name, Attribute.ACTION.getName())) {
+      return Level.THIRD;
+    } else if (Attribute.isAttribute(name)) {
+      return Level.FIFTH;
+    } else if (name.startsWith(STATEMENT)) {
+      return Level.FIRST;
+    }
+
+    switch (name) {
+      case ROW:
+        return Level.FIRST;
+      case DB_TABLE_NAME:
+        return Level.SECOND;
+      case ACCESS:
+      case KEY:
+      case TABLE:
+        return Level.THIRD;
+      default:
+        return Level.FOURTH;
+    }
+  }
+
+  private Category getProperCategory(String name) {
+    if (Attribute.isAttribute(name)) {
+      return Category.ATTRIBUTE;
+    }
+    return Category.ELEMENT;
+  }
+
+  private Occurrence getProperOccurrence(String name) {
+    if (Attribute.isAttribute(name)) {
+      return Occurrence.ofOptional();
+    } else if (Objects.equals(name, ROW) || Objects.equals(name, STATEMENT)) {
+      return Occurrence.ofZeroUnbounded();
+    } else if (
+        Objects.equals(name, DB_TABLE_NAME)
+            || Objects.equals(name, ACCESS)
+            || Objects.equals(name, KEY)
+            || Objects.equals(name, TABLE)
+    ) {
+      return Occurrence.ofOnlyOne();
+    }
+    return Occurrence.ofZeroOne();
+  }
+
+  public int getLevel() {
+    return level;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public Category getCategory() {
+    return category;
+  }
+
+  public Type getType() {
+    return type;
+  }
+
+  public Occurrence getOccurrence() {
+    return occurrence;
+  }
+
+  public void setCategory(Category changedCategory) {
+    this.category = changedCategory;
+  }
 }
