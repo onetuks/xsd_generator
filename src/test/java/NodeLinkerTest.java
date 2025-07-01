c931ff3686662f3fc67b837e1c8f33fd610bdcb7
onetuks <onetuks@naver.com>
2025-07-01 20:12:49 +0900

    Feat: DTO -> Domain 링커 기능 구현

--- src/test/java/NodeLinkerTest.java
+++ src/test/java/NodeLinkerTest.java
@@ -1,4 +1,72 @@
-package PACKAGE_NAME;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import core.NodeLinker;
+import dto.XDataTypes;
+import model.XTree;
+import org.junit.jupiter.api.Test;
 
 public class NodeLinkerTest {
+
+  @Test
+  public void linkTest() {
+    // given
+    NodeLinker linker = new NodeLinker();
+    XDataTypes dataTypes = TestFixture.createXDataTypes();
+    XTree expected = TestFixture.createXTree();
+
+    // when
+    XTree result = linker.link(dataTypes);
+
+    // then
+    assertEquals(expected.getNamespace(), result.getNamespace());
+    assertEquals(expected.getFilePath(), result.getFilePath());
+    assertEquals(expected.getRoot().getEntity().getName(), result.getRoot().getEntity().getName());
+    assertEquals(
+        expected.getRoot()
+            .getChildren().get(0).getEntity().getName(),
+        result.getRoot()
+            .getChildren().get(0).getEntity().getName());
+    assertEquals(
+        expected.getRoot()
+            .getChildren().get(0)
+            .getChildren().get(0).getEntity().getName(),
+        result.getRoot()
+            .getChildren().get(0)
+            .getChildren().get(0).getEntity().getName());
+    assertEquals(
+        expected.getRoot()
+            .getChildren().get(0)
+            .getChildren().get(1).getEntity().getName(),
+        result.getRoot()
+            .getChildren().get(0)
+            .getChildren().get(1).getEntity().getName());
+    assertEquals(
+        expected.getRoot()
+            .getChildren().get(0)
+            .getChildren().get(2).getEntity().getName(),
+        result.getRoot()
+            .getChildren().get(0)
+            .getChildren().get(2).getEntity().getName());
+    assertEquals(
+        expected.getRoot()
+            .getChildren().get(1)
+            .getChildren().get(0).getEntity().getName(),
+        result.getRoot()
+            .getChildren().get(1)
+            .getChildren().get(0).getEntity().getName());
+    assertEquals(
+        expected.getRoot()
+            .getChildren().get(1)
+            .getChildren().get(1).getEntity().getName(),
+        result.getRoot()
+            .getChildren().get(1)
+            .getChildren().get(1).getEntity().getName());
+    assertEquals(
+        expected.getRoot()
+            .getChildren().get(1)
+            .getChildren().get(2).getEntity().getName(),
+        result.getRoot()
+            .getChildren().get(1)
+            .getChildren().get(2).getEntity().getName());
+  }
 }
