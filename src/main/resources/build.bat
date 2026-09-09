d6dd1a4da8d9765c74a729ccb7545d070b014542
onetuks <onetuks@naver.com>
2026-09-09 17:47:54 +0900

    deploy: jpackage 배포 시 wix 경로 추가

--- src/main/resources/build.bat
+++ src/main/resources/build.bat
@@ -1,9 +1,11 @@
-"C:\Users\seyou\.jdks\temurin-21.0.8\bin\jpackage.exe" ^
+set "PATH=%PATH%;C:\Program Files (x86)\WiX Toolset v3.14\bin"
+
+"C:\Users\onetu\.jdks\ms-21.0.10\bin\jpackage.exe" ^
   --name xsd_generator ^
-  --app-version 1.0 ^
+  --app-version 2.0 ^
   --input build\libs ^
-  --main-jar xsd_generator-1.0.jar ^
-  --main-class MainFrame ^
+  --main-jar xsd_generator-2.0.jar ^
+  --main-class Main ^
   --type exe ^
   --win-shortcut ^
   --win-menu ^
